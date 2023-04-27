3ab845e80dd85f9bdb68fb316c7ff55575c2eb3f
uraflower <82873315+uraflower@users.noreply.github.com>
2023-04-27 20:02:23 +0900

    ➕ Add 4.02__Add smart add page file

---
+++ lib/pages/smart_add_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SmartAdd extends StatelessWidget {
+  const SmartAdd({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// 카메라 ->  ...   ->  로딩 -> 스마트 등록
+//  ...   -> 갤러리 ->  ...
+
+class Camera extends StatefulWidget {
+  const Camera({super.key});
+
+  @override
+  State<Camera> createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
