918f0ab0e264c795a3320781a6d9a8ce673cf521
uraflower <82873315+uraflower@users.noreply.github.com>
2023-04-27 21:56:42 +0900

    ➖ Remove 4.02__Remove Camera class from smart add page

--- lib/pages/smart_add_page.dart
+++ lib/pages/smart_add_page.dart
@@ -8,20 +8,3 @@ class SmartAdd extends StatelessWidget {
     return Container();
   }
 }
-
-// 카메라 ->  ...   ->  로딩 -> 스마트 등록
-//  ...   -> 갤러리 ->  ...
-
-class Camera extends StatefulWidget {
-  const Camera({super.key});
-
-  @override
-  State<Camera> createState() => _CameraState();
-}
-
-class _CameraState extends State<Camera> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
