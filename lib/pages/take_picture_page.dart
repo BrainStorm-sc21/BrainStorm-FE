741b08d25a697ce0ac2f49164f624dc6bc510d4d
uraflower <82873315+uraflower@users.noreply.github.com>
2023-04-27 20:53:52 +0900

    ➕ Add 4.02__Add take picture page

---
+++ lib/pages/take_picture_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// 카메라 ->  ...   ->  로딩 -> 스마트 등록
+//  ...   -> 갤러리 ->  ...
+class TakePicture extends StatefulWidget {
+  const TakePicture({super.key});
+
+  @override
+  State<TakePicture> createState() => _TakePictureState();
+}
+
+class _TakePictureState extends State<TakePicture> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
