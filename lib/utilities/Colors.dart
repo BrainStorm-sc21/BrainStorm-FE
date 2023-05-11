28d5aad770e28068a1b01516fc2c41ee73dfbbdb
joongwon <wnddnjs823@ajou.ac.kr>
2023-05-11 16:50:39 +0900

    🔨 Update Color styles

--- lib/utilities/Colors.dart
+++ lib/utilities/Colors.dart
@@ -5,7 +5,10 @@ class ColorStyles {
   static const Color shareColor = Color.fromRGBO(212, 117, 73, 1);
   static const Color exchangColor = Color.fromRGBO(70, 200, 218, 1);
   static const Color groupBuyColor = Color.fromRGBO(159, 97, 200, 1);
-  static const Color black = Colors.black;
+  static const Color borderColor = Color.fromRGBO(89, 89, 89, 0.2);
+  static const Color hintTextColor = Color.fromRGBO(149, 149, 149, 0.5);
+  static const Color transparentColor = Color.fromARGB(0, 0, 0, 0);
+  static const Color black = Color.fromARGB(255, 0, 0, 0);
   static const Color white = Colors.white;
   static const Color cyan = Colors.cyan;
   static Color errorRed = Colors.redAccent.shade700;
