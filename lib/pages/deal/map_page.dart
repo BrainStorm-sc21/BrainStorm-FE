7ca486893c26b8d6d7e655aaa8d95a5381baf866
MinJu Jo <pida6716@gmail.com>
2023-05-14 17:50:58 +0900

    ➕ add 8.02_map marker

--- lib/pages/deal/map_page.dart
+++ lib/pages/deal/map_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:naver_map_plugin/naver_map_plugin.dart';
@@ -14,64 +15,56 @@ class _MapPageState extends State<MapPage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   Completer<NaverMapController> _controller = Completer();
 
-  final LocationTrackingMode _trackingMode = LocationTrackingMode.NoFollow;
+  final List<Marker> _markers = [];
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      OverlayImage.fromAssetImage(
+        assetName: 'icon/marker.png',
+        devicePixelRatio: window.devicePixelRatio,
+      ).then((image) {
+        setState(() {
+          _markers.add(Marker(
+              markerId: 'id',
+              position: const LatLng(37.566570, 126.978442),
+              captionText: "커스텀 아이콘",
+              captionColor: Colors.indigo,
+              captionTextSize: 20.0,
+              alpha: 0.8,
+              captionOffset: 30,
+              //icon: image,
+              anchor: AnchorPoint(0.5, 1),
+              width: 45,
+              height: 45,
+              infoWindow: '인포 윈도우',
+              onMarkerTab: _onMarkerTap));
+        });
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      body: NaverMap(
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(37.566570, 126.978442),
-          zoom: 17,
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            _naverMap(),
+          ],
         ),
-        onMapCreated: onMapCreated,
-        mapType: MapType.Basic,
-        initLocationTrackingMode: _trackingMode,
-        //locationButtonEnable: true,
-        indoorEnable: true,
-        onCameraIdle: _onCameraIdle,
-        onMapTap: _onMapTap,
-        onMapLongTap: _onMapLongTap,
-        onMapDoubleTap: _onMapDoubleTap,
-        onMapTwoFingerTap: _onMapTwoFingerTap,
-        maxZoom: 17,
-        minZoom: 15,
       ),
     );
   }
 
-  _onMapTap(LatLng position) async {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text('[onTap] lat: ${position.latitude}, lon: ${position.longitude}'),
-      duration: const Duration(milliseconds: 500),
-      backgroundColor: Colors.black,
-    ));
-  }
-
-  _onMapLongTap(LatLng position) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text('[onLongTap] lat: ${position.latitude}, lon: ${position.longitude}'),
-      duration: const Duration(milliseconds: 500),
-      backgroundColor: Colors.black,
-    ));
-  }
-
-  _onMapDoubleTap(LatLng position) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text('[onDoubleTap] lat: ${position.latitude}, lon: ${position.longitude}'),
-      duration: const Duration(milliseconds: 500),
-      backgroundColor: Colors.black,
-    ));
-  }
-
-  _onMapTwoFingerTap(LatLng position) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text('[onTwoFingerTap] lat: ${position.latitude}, lon: ${position.longitude}'),
-      duration: const Duration(milliseconds: 500),
-      backgroundColor: Colors.black,
-    ));
-  }
+  // _onMapTap(LatLng position) async {
+  //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+  //     content: Text('[onTap] lat: ${position.latitude}, lon: ${position.longitude}'),
+  //     duration: const Duration(milliseconds: 500),
+  //     backgroundColor: Colors.black,
+  //   ));
+  // }
 
   /// 지도 생성 완료시
   void onMapCreated(NaverMapController controller) {
@@ -79,13 +72,47 @@ class _MapPageState extends State<MapPage> {
     _controller.complete(controller);
   }
 
-  /// my location button
-  // void _onTapLocation() async {
-  //   final controller = await _controller.future;
-  //   controller.setLocationTrackingMode(LocationTrackingMode.Follow);
-  // }
+  _naverMap() {
+    return Expanded(
+        child: NaverMap(
+      initialCameraPosition: const CameraPosition(
+        target: LatLng(37.566570, 126.978442),
+        zoom: 17,
+      ),
+      onMapCreated: onMapCreated,
+      mapType: MapType.Basic,
+      indoorEnable: true,
+      markers: _markers,
+      onMapTap: _onMapTap,
+      maxZoom: 17,
+      minZoom: 15,
+    ));
+  }
 
-  void _onCameraIdle() {
-    print('카메라 움직임 멈춤');
+  // ================== method ==========================
+
+  void _onMapTap(LatLng latLng) {
+    OverlayImage.fromAssetImage(
+      assetName: 'assets/images/임시공구마커.png',
+      devicePixelRatio: window.devicePixelRatio,
+    ).then((image) {
+      _markers.add(Marker(
+        markerId: DateTime.now().toIso8601String(),
+        //icon: image,
+        position: latLng,
+        onMarkerTab: _onMarkerTap,
+      ));
+      setState(() {});
+    });
+  }
+
+  void _onMarkerTap(Marker? marker, Map<String, int?> iconSize) {
+    int pos = _markers.indexWhere((m) => m.markerId == marker!.markerId);
+    setState(() {
+      _markers[pos].captionText = '선택됨';
+    });
+    setState(() {
+      _markers.removeWhere((m) => m.markerId == marker!.markerId);
+    });
   }
 }
