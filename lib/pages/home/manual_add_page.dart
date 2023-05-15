ab01ac048e663f41770a2ef0d570489d29168cd9
uraflower <82873315+uraflower@users.noreply.github.com>
2023-05-15 15:17:10 +0900

    ♻️ Refactor 4.01_Format code

--- lib/pages/home/manual_add_page.dart
+++ lib/pages/home/manual_add_page.dart
@@ -22,7 +22,8 @@ class _ManualAddPageState extends State<ManualAddPage> {
   // Food 모델 인스턴스 생성
   late Food food;
   // 수량 입력란에 실제로 표시되는 text를 갖는 컨트롤러
-  late final TextEditingController _stockStringController = TextEditingController();
+  late final TextEditingController _stockStringController =
+      TextEditingController();
   // 수량 입력란 focus가 컨트롤러에 의해 꼬이지 않도록 focus를 고정해주는 focusNode
   late final FocusNode _stockFocusNode = FocusNode();
 
@@ -50,7 +51,8 @@ class _ManualAddPageState extends State<ManualAddPage> {
   void setName(String value) => setState(() => food.foodName = value);
   void setStorage(String value) => setState(() => food.storageWay = value);
   void setStock(num value) => setState(() => food.stock = value);
-  void setExpireDate(DateTime value, {int? index}) => setState(() => food.expireDate = value);
+  void setExpireDate(DateTime value, {int? index}) =>
+      setState(() => food.expireDate = value);
   // 수량 입력란에 stock 값이 표시되도록 set state
   void updateControllerText() =>
       setState(() => _stockStringController.text = food.stock.toString());
@@ -78,16 +80,23 @@ class _ManualAddPageState extends State<ManualAddPage> {
         delegate: SliverChildListDelegate([
           FoodName(setName: setName), // 식료품 이름 입력
           const SizedBox(height: 30), // 여백
-          FoodStorage(storage: food.storageWay, setStorage: setStorage), // 식료품 보관장소 선택
+          FoodStorage(
+            storage: food.storageWay,
+            setStorage: setStorage,
+          ), // 식료품 보관장소 선택
           divider,
           FoodStockTextfield(
-              stock: food.stock,
-              setStock: setStock,
-              controller: _stockStringController,
-              updateControllerText: updateControllerText,
-              focusNode: _stockFocusNode), // 식료품 수량 조절
+            stock: food.stock,
+            setStock: setStock,
+            controller: _stockStringController,
+            updateControllerText: updateControllerText,
+            focusNode: _stockFocusNode,
+          ), // 식료품 수량 조절
           divider,
-          FoodExpireDate(expireDate: food.expireDate, setExpireDate: setExpireDate), // 식료품 소비기한 입력
+          FoodExpireDate(
+            expireDate: food.expireDate,
+            setExpireDate: setExpireDate,
+          ), // 식료품 소비기한 입력
         ]),
       ),
     );
@@ -152,7 +161,8 @@ class _FoodNameState extends State<FoodName> {
       child: TextFormField(
         decoration: InputDecoration(
           hintText: '식품 이름을 입력하세요',
-          contentPadding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
           enabledBorder: const OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(20)),
             borderSide: BorderSide(
@@ -212,7 +222,7 @@ class FoodStorage extends StatelessWidget {
     return Column(
       children: [
         // "보관장소" title
-        Row(crossAxisAlignment: CrossAxisAlignment.center, children: const [
+        const Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
           Text("보관장소"),
           Spacer(),
         ]),
@@ -225,9 +235,12 @@ class FoodStorage extends StatelessWidget {
               text: _storages[index],
               onPressed: () => setStorage(_storages[index]),
               width: MediaQuery.of(context).size.width / 3.0 - 50,
-              backgroundColor:
-                  storage == _storages[index] ? ColorStyles.mainColor : ColorStyles.white,
-              foregroundColor: storage == _storages[index] ? ColorStyles.white : ColorStyles.black,
+              backgroundColor: storage == _storages[index]
+                  ? ColorStyles.mainColor
+                  : ColorStyles.white,
+              foregroundColor: storage == _storages[index]
+                  ? ColorStyles.white
+                  : ColorStyles.black,
               borderColor: ColorStyles.mainColor,
             );
           }),
