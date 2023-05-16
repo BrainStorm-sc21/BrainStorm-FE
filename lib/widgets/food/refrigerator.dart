e9b19307a79b6369cbd634842c3a619ce8163e7c
MinJu Jo <pida6716@gmail.com>
2023-05-16 16:07:33 +0900

    🔨 Update 3.02_Change name of food

--- lib/widgets/food/refrigerator.dart
+++ lib/widgets/food/refrigerator.dart
@@ -16,23 +16,40 @@ class Refrigerator extends StatefulWidget {
 class _RefrigeratorState extends State<Refrigerator> {
   late Food food;
   List<Food> foodList = List.empty(growable: true);
+
+  List<Food> exampleFood = [
+    Food(
+        foodId: 0,
+        foodName: "토마토",
+        storageWay: "냉장",
+        stock: 2,
+        expireDate: DateFormat('yyyy-MM-dd').parse('${DateTime.now()}')),
+    Food(
+        foodId: 1,
+        foodName: "감자",
+        storageWay: "실온",
+        stock: 5,
+        expireDate: DateFormat('yyyy-MM-dd').parse('${DateTime.now()}')),
+    Food(
+        foodId: 2,
+        foodName: "가지",
+        storageWay: "냉동",
+        stock: 1,
+        expireDate: DateFormat('yyyy-MM-dd').parse('${DateTime.now()}')),
+    Food(
+        foodId: 3,
+        foodName: "버섯",
+        storageWay: "냉장",
+        stock: 4,
+        expireDate: DateFormat('yyyy-MM-dd').parse('${DateTime.now()}'))
+  ];
   List<bool> absorbBool = List.filled(100, true, growable: true);
+
+  final List<TextEditingController> _foodNameController = [];
 
   @override
   void initState() {
     super.initState();
-
-    var now = DateFormat('yyyy-MM-dd').parse('${DateTime.now()}');
-    foodList = [
-      Food(foodId: 0, foodName: "토마토", storageWay: "냉장", stock: 2, expireDate: now),
-      Food(foodId: 1, foodName: "감자", storageWay: "실온", stock: 5, expireDate: now),
-      Food(foodId: 2, foodName: "가지", storageWay: "냉동", stock: 1, expireDate: now),
-      Food(foodId: 3, foodName: "버섯", storageWay: "냉장", stock: 4, expireDate: now),
-    ];
-
-    if (widget.storage != '전체') {
-      foodList = foodList.where((foodStorage) => foodStorage.storageWay == widget.storage).toList();
-    }
 
     food = Food(
       foodId: 1,
@@ -41,12 +58,48 @@ class _RefrigeratorState extends State<Refrigerator> {
       stock: 1,
       expireDate: DateFormat('yyyy-MM-dd').parse('${DateTime.now()}'),
     );
+
+    initFoods();
+    initController();
   }
 
   void setStock(int index, num value) => setState(() => foodList[index].stock = value);
   void setStorage(int index, String value) => setState(() => foodList[index].storageWay = value);
   void setExpireDate(DateTime value, {int? index}) =>
       setState(() => foodList[index!].expireDate = value);
+
+  void initFoods() {
+    for (var fooditem in exampleFood) {
+      if (widget.storage == "전체" || widget.storage == fooditem.storageWay) {
+        foodList.add(Food(
+          foodId: fooditem.foodId,
+          foodName: fooditem.foodName,
+          stock: fooditem.stock,
+          storageWay: fooditem.storageWay,
+          expireDate: fooditem.expireDate,
+        ));
+      }
+    }
+  }
+
+  void initController() {
+    for (int index = 0; index < foodList.length; index++) {
+      _foodNameController.add(TextEditingController());
+      _foodNameController[index].text = foodList[index].foodName;
+    }
+  }
+
+  @override
+  void dispose() {
+    disposeController();
+    super.dispose();
+  }
+
+  void disposeController() {
+    for (var controller in _foodNameController) {
+      controller.dispose();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,13 +116,22 @@ class _RefrigeratorState extends State<Refrigerator> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    food.foodName,
-                    style: const TextStyle(
-                        fontSize: 20,
-                        color: Color.fromARGB(255, 65, 60, 60),
-                        fontWeight: FontWeight.bold),
-                  ),
+                  AbsorbPointer(
+                      absorbing: absorbBool[index],
+                      child: SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.75,
+                          child: TextField(
+                            controller: _foodNameController[index],
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              counterText: '',
+                            ),
+                            style: const TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            maxLength: 20,
+                          ))),
                   progressBars(context),
                 ],
               ),
