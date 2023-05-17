f2f5b6d43f5ed0dc14a0caafebf3cbb0f80199c0
MinJu Jo <pida6716@gmail.com>
2023-05-17 14:51:54 +0900

    🐛 Fix 8.01_Long titles go beyond space

--- lib/pages/deal/trading_board_page.dart
+++ lib/pages/deal/trading_board_page.dart
@@ -1,8 +1,11 @@
+import 'package:brainstorm_meokjang/models/deal.dart';
 import 'package:brainstorm_meokjang/utilities/colors.dart';
 import 'package:flutter/material.dart';
 
 class TradingBoard extends StatelessWidget {
-  const TradingBoard({super.key});
+  const TradingBoard({super.key, required this.posts});
+
+  final List<Deal> posts;
 
   final Map dealColors = const {
     '공구': ColorStyles.groupBuyColor,
@@ -10,51 +13,19 @@ class TradingBoard extends StatelessWidget {
     '나눔': ColorStyles.shareColor
   };
 
-  final List<Map<String, String>> post = const [
-    {
-      "name": "감자 공동구매 하실 분!",
-      "distance": "150M",
-      "dealType": "공구",
-      "imgUrl":
-          "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcR1M89lNmXLBltfEc5TQZJSpcqvZ36vvZyZfpP98EFh-i4Q9X8S8woN6El91b1pZ5Sw",
-      "time": "30분 전"
-    },
-    {
-      "name": "양파 나눔해요~",
-      "distance": "400M",
-      "dealType": "교환",
-      "imgUrl":
-          "https://i.namu.wiki/i/qTfdtopPV7GKQ0YmjVsHxythtmlSQ35OppjcjwJgHJoLVXzx5iCZRFHaq-mXoTR5cl-j2X4SQm1xvyj2hhxBEw.webp",
-      "time": "1시간 전"
-    },
-    {
-      "name": "이천 쌀 공구하실 분 구합니다!!!",
-      "distance": "1.2KM",
-      "dealType": "나눔",
-      "imgUrl": "https://www.newspeak.kr/news/photo/202209/435707_284048_3504.jpg",
-      "time": "2시간 전"
-    },
-    {
-      "name": "사과랑 바나나 교환해요",
-      "distance": "750M",
-      "dealType": "공구",
-      "imgUrl":
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRlxKlz8H0tHG-DpyUhBOOo6wpGw_NnEYPMLDjrfVA3aSPyIdCfmzS_fzOcnj0seChhGo&usqp=CAU",
-      "time": "4시간 전"
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
+    Deal deal;
+
     return ListView.builder(
-      itemCount: 100,
+      itemCount: posts.length,
       itemBuilder: (context, index) {
-        final item = post[index % post.length];
-        final name = item["name"] ?? "name";
-        final distance = item["distance"] ?? "distance";
-        final dealType = item["dealType"] ?? "dealType";
-        final time = item["time"] ?? "time";
-        final imgUrl = item["imgUrl"] ?? "";
+        deal = posts[index];
+        final dealName = deal.dealName;
+        final distance = deal.distance;
+        final dealType = deal.dealType;
+        final time = deal.dealTime;
+        final imgUrl = deal.dealImage;
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
           child: Row(
@@ -91,10 +62,11 @@ class TradingBoard extends StatelessWidget {
                   SizedBox(
                     width: 270,
                     child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                      Text(name,
-                          style: const TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w500, height: 1)),
-                      //const Spacer(),
+                      Flexible(
+                          child: Text(dealName,
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500, height: 1),
+                              overflow: TextOverflow.ellipsis)),
                       Text(time,
                           style: const TextStyle(fontSize: 13, color: Colors.grey, height: 1)),
                     ]),
