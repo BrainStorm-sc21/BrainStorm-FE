9dec4d2016feac13d624b467cc6d5f66fb29f5be
MinJu Jo <pida6716@gmail.com>
2023-05-21 23:15:29 +0900

    🔨 Update 9.02_deal search

--- lib/pages/deal/deal_page.dart
+++ lib/pages/deal/deal_page.dart
@@ -27,18 +27,99 @@ class _DealPageState extends State<DealPage> {
   final List<String> _valueList = ['거리순', '최신순'];
   String _selectedValue = '거리순';
 
+  // void setDeal(int dealType) => setState(() {
+  //       _checkDeal[dealType] = !_checkDeal[dealType];
+  //       if (_checkDeal.contains(true)) {
+  //         posts = dealData.data.where((deal) => _checkDeal[deal.dealType] == true).toList();
+  //       } else {
+  //         posts = dealData.data;
+  //       }
+  //     });
+
   void setDeal(int dealType) => setState(() {
         _checkDeal[dealType] = !_checkDeal[dealType];
         if (_checkDeal.contains(true)) {
-          posts = dealData.data.where((deal) => _checkDeal[deal.dealType] == true).toList();
+          posts = entirePosts.where((deal) => _checkDeal[deal.dealType] == true).toList();
         } else {
-          posts = dealData.data;
+          posts = entirePosts;
         }
       });
-  void setSearch() => setState(() {});
+
+  void setSearch(text) => setState(() {
+        print("검색버튼클릭");
+        if (text == Null) {
+          posts = entirePosts;
+        } else {
+          posts = entirePosts
+              .where((deal) => deal.dealName.contains(_textEditingController.text))
+              .toList();
+        }
+      });
   void setdropdown(String selectedValue, String value) => setState(() => selectedValue = value);
 
   final TextEditingController _textEditingController = TextEditingController();
+
+  List<Deal> entirePosts = [
+    Deal(
+        userId: 1,
+        dealId: 0,
+        dealName: "감자 공동구매 하실 분!",
+        dealType: 0,
+        distance: 150,
+        dealContent: "~~",
+        latitude: 37.284159,
+        longitude: 127.044608,
+        createdAt: DateTime.parse('2023-05-21T11:58:20.551705'),
+        dealImage1:
+            'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcR1M89lNmXLBltfEc5TQZJSpcqvZ36vvZyZfpP98EFh-i4Q9X8S8woN6El91b1pZ5Sw'),
+    Deal(
+        userId: 2,
+        dealId: 1,
+        dealName: "양파 나눔해요~",
+        dealType: 1,
+        distance: 400,
+        dealContent: "양파 나눔 내용",
+        latitude: 37.2840,
+        longitude: 127.044607,
+        createdAt: DateTime.parse('2023-05-21 11:22:50.030315'),
+        dealImage1:
+            'https://i.namu.wiki/i/qTfdtopPV7GKQ0YmjVsHxythtmlSQ35OppjcjwJgHJoLVXzx5iCZRFHaq-mXoTR5cl-j2X4SQm1xvyj2hhxBEw.webp'),
+    Deal(
+        userId: 3,
+        dealId: 2,
+        dealName: "이천 쌀 공구하실 분 구합니다!!",
+        dealType: 0,
+        distance: 1200,
+        dealContent: "이천 쌀 공구 내용",
+        latitude: 37.284049,
+        longitude: 127.041648,
+        createdAt: DateTime.parse('2023-05-20T10:01:20.551705'),
+        dealImage1: 'https://www.newspeak.kr/news/photo/202209/435707_284048_3504.jpg'),
+    Deal(
+        userId: 4,
+        dealId: 3,
+        dealName: "사과랑 바나나 교환해요",
+        dealType: 1,
+        distance: 750,
+        dealContent: "사과 바나나 교환 내용",
+        latitude: 37.289361,
+        longitude: 127.044601,
+        createdAt: DateTime.parse('2023-05-18T11:58:20.551705'),
+        dealImage1:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRlxKlz8H0tHG-DpyUhBOOo6wpGw_NnEYPMLDjrfVA3aSPyIdCfmzS_fzOcnj0seChhGo&usqp=CAU'),
+    Deal(
+        userId: 5,
+        dealId: 4,
+        dealName: "이건 정말 긴 제목을 가지고 있는 게시물의 테스트를 위한 더미값입니다",
+        dealType: 2,
+        distance: 900,
+        dealContent:
+            "이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.이건 정말 긴 내용을 가지고 있는 게시물의 테스트를 위한 더미값입니다.",
+        latitude: 37.2839598,
+        longitude: 127.044599,
+        createdAt: DateTime.parse('2023-05-19T11:58:20.551705'),
+        dealImage1: 'https://i.pinimg.com/originals/b0/df/95/b0df95cfc6f31293d002d4d6daac253c.jpg')
+  ];
 
   List<Deal> posts = List.empty(growable: true);
   late DealData dealData;
@@ -55,13 +136,13 @@ class _DealPageState extends State<DealPage> {
       print("Deal Status: ${resp.statusCode}");
       print("Data:\n${resp.data}");
 
-      DealData dealData = DealData.fromJson(resp.data);
+      // DealData dealData = DealData.fromJson(resp.data);
 
-      setState(() {
-        for (Deal dealitem in dealData.data) {
-          posts.add(dealitem);
-        }
-      });
+      // setState(() {
+      //   for (Deal dealitem in dealData.data) {
+      //     posts.add(dealitem);
+      //   }
+      // });
     } catch (e) {
       Exception(e);
     } finally {
@@ -74,6 +155,22 @@ class _DealPageState extends State<DealPage> {
   void initState() {
     super.initState();
     getServerDealDataWithDio();
+    initDeals();
+  }
+
+  void initDeals() {
+    for (var deal in entirePosts) {
+      posts.add(Deal(
+          userId: deal.userId,
+          dealName: deal.dealName,
+          dealType: deal.dealType,
+          distance: deal.distance,
+          dealContent: deal.dealContent,
+          latitude: deal.latitude,
+          longitude: deal.longitude,
+          createdAt: deal.createdAt,
+          dealImage1: deal.dealImage1));
+    }
   }
 
   @override
@@ -104,7 +201,7 @@ class _DealPageState extends State<DealPage> {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             CustomSearchBar(
-              onTap: () => setSearch(),
+              onTap: () => setSearch(_textEditingController),
               fillColor: ColorStyles.translucent,
               borderColor: ColorStyles.black,
               textEditingController: _textEditingController,
@@ -123,7 +220,9 @@ class _DealPageState extends State<DealPage> {
                               height: 30,
                               text: DealType.dealTypeName[index],
                               fontSize: 15,
-                              onPressed: () => dealData.data.isEmpty ? {} : setDeal(index),
+                              onPressed: () =>
+                                  //dealData.data.isEmpty ? print("값 없음") : setDeal(index),
+                                  entirePosts.isEmpty ? print("값 없음") : setDeal(index),
                               backgroundColor: _checkDeal[index]
                                   ? DealType.dealTextColors[index]
                                   : ColorStyles.white,
@@ -176,7 +275,8 @@ class _DealPageState extends State<DealPage> {
                 if (_selectedValue == '거리순') {
                   posts.sort((a, b) => a.distance.compareTo(b.distance));
                 } else if (_selectedValue == '최신순') {
-                  posts.sort((a, b) => a.createdAt.compareTo(b.createdAt));
+                  posts.sort((b, a) => a.createdAt.compareTo(b.createdAt));
+                  posts.reversed;
                 }
               });
             },
