a78a36d01640f4cfba9dbc09ca1c86a871c95601
MinJu Jo <pida6716@gmail.com>
2023-05-24 13:25:04 +0900

    ➕ Add 27.02_recipe button

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:brainstorm_meokjang/pages/home/smart_add_page.dart';
 import 'package:brainstorm_meokjang/utilities/colors.dart';
 import 'package:brainstorm_meokjang/utilities/domain.dart';
 import 'package:brainstorm_meokjang/widgets/food/refrigerator.dart';
+import 'package:brainstorm_meokjang/widgets/rounded_outlined_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:dio/dio.dart';
@@ -59,7 +60,6 @@ class _HomePageState extends State<HomePage> {
       FoodData foodData = FoodData.fromJson(resp.data);
 
       print("Food Status: ${resp.statusCode}");
-      print("FoodData : ${resp.data}");
 
       setState(() {
         for (Food fooditem in foodData.data) {
@@ -81,19 +81,14 @@ class _HomePageState extends State<HomePage> {
   }
 
   TabBar get _tabBar => const TabBar(
-          padding: EdgeInsets.only(top: 10),
-          isScrollable: false,
-          indicatorColor: ColorStyles.mainColor,
-          indicatorWeight: 4,
-          labelColor: ColorStyles.mainColor,
-          unselectedLabelColor: ColorStyles.textColor,
-          labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-          tabs: [
-            Tab(text: "전체"),
-            Tab(text: "냉장"),
-            Tab(text: "냉동"),
-            Tab(text: "실온")
-          ]);
+      padding: EdgeInsets.only(top: 10),
+      isScrollable: false,
+      indicatorColor: ColorStyles.mainColor,
+      indicatorWeight: 4,
+      labelColor: ColorStyles.mainColor,
+      unselectedLabelColor: ColorStyles.textColor,
+      labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+      tabs: [Tab(text: "전체"), Tab(text: "냉장"), Tab(text: "냉동"), Tab(text: "실온")]);
 
   @override
   Widget build(BuildContext context) {
@@ -115,32 +110,34 @@ class _HomePageState extends State<HomePage> {
                   flexibleSpace: Container(
                       decoration: const BoxDecoration(
                           borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(15),
-                              bottomRight: Radius.circular(15)),
+                              bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
                           gradient: LinearGradient(
                               begin: Alignment.topLeft,
                               end: Alignment.bottomRight,
-                              colors: <Color>[
-                                ColorStyles.darkmainColor,
-                                ColorStyles.mainColor
-                              ]))),
+                              colors: <Color>[ColorStyles.darkmainColor, ColorStyles.mainColor]))),
                   actions: [
                     IconButton(
                         padding: const EdgeInsets.only(top: 28, right: 30),
-                        icon: const Icon(Icons.notifications,
-                            color: Colors.white, size: 30),
+                        icon: const Icon(Icons.notifications, color: Colors.white, size: 30),
                         onPressed: () {
                           print("우측 상단 검색 아이콘 클릭 됨");
                         })
                   ]),
             ),
-            body: Column(children: [
+            body: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
               _tabBar,
-              const Divider(
-                  height: 0,
-                  color: ColorStyles.lightgrey,
-                  thickness: 1.5,
-                  endIndent: 10),
+              const Divider(height: 0, color: ColorStyles.lightgrey, thickness: 1.5, endIndent: 10),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 13, 10),
+                child: RoundedOutlinedButton(
+                    height: 33,
+                    backgroundColor: ColorStyles.cream,
+                    borderColor: ColorStyles.cream,
+                    foregroundColor: ColorStyles.textColor,
+                    onPressed: () {},
+                    fontSize: 14,
+                    text: "🧑🏻‍🍳 레시피 추천받기"),
+              ),
               Expanded(
                   child: foodList.isEmpty
                       ? const Center(child: Text("냉장고에 재료를 추가해주세요!"))
@@ -174,8 +171,7 @@ Widget? floatingButtons(BuildContext context) {
           child: const Icon(Icons.camera_alt, color: Colors.white),
           backgroundColor: ColorStyles.mainColor,
           onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const SmartAddPage()));
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const SmartAddPage()));
           }),
       SpeedDialChild(
         child: const Icon(
@@ -184,8 +180,7 @@ Widget? floatingButtons(BuildContext context) {
         ),
         backgroundColor: ColorStyles.mainColor,
         onTap: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const ManualAddPage()));
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const ManualAddPage()));
         },
       )
     ],
