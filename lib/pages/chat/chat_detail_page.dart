a0c816b02a528092330a31a80dddc2fb9cfe5618
uraflower <82873315+uraflower@users.noreply.github.com>
2023-05-27 00:45:06 +0900

    🔨 Update 14.03_Send and show message

--- lib/pages/chat/chat_detail_page.dart
+++ lib/pages/chat/chat_detail_page.dart
@@ -1,8 +1,10 @@
 import 'package:brainstorm_meokjang/utilities/colors.dart';
 import 'package:brainstorm_meokjang/widgets/rounded_outlined_button.dart';
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
-class ChatDetailPage extends StatelessWidget {
+class ChatDetailPage extends StatefulWidget {
   final String nickname;
   final String content;
   const ChatDetailPage({
@@ -10,6 +12,36 @@ class ChatDetailPage extends StatelessWidget {
     required this.nickname,
     required this.content,
   });
+
+  @override
+  State<ChatDetailPage> createState() => _ChatDetailPageState();
+}
+
+class _ChatDetailPageState extends State<ChatDetailPage> {
+  final TextEditingController _controller = TextEditingController();
+  // final WebSocketChannel _client = IOWebSocketChannel.connect('ws://meokjang.com/chat');
+  final WebSocketChannel _client = IOWebSocketChannel.connect(
+    Uri.parse('wss://echo.websocket.events'),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _client.sink.close();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void sendMessage() {
+    if (_controller.text.trim().isNotEmpty) {
+      _client.sink.add(_controller.text);
+      _controller.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +62,7 @@ class ChatDetailPage extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 ),
                 Text(
-                  nickname,
+                  widget.nickname,
                   style: const TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.w600,
@@ -44,10 +76,24 @@ class ChatDetailPage extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
+          // 채팅 기록
+          StreamBuilder(
+            stream: _client.stream,
+            builder: (context, snapshot) {
+              return Container(
+                color: ColorStyles.lightgrey,
+                child: Text(
+                  snapshot.hasData ? '${snapshot.data}' : '',
+                  style: const TextStyle(
+                    color: ColorStyles.black,
+                  ),
+                ),
+              );
+            },
+          ),
+          // 하단 바
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Container(
               color: ColorStyles.white,
               child: Row(
@@ -73,11 +119,24 @@ class ChatDetailPage extends StatelessWidget {
                     ),
                   ),
                   // 메시지 입력 창
-
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                          hintText: '메시지를 입력하세요',
+                          hintStyle: TextStyle(
+                            color: ColorStyles.textColor,
+                          ),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 8.0,
+                          )),
+                    ),
+                  ),
                   // 전송 버튼
                   RoundedOutlinedButton(
                     text: '전송',
-                    onPressed: () {},
+                    onPressed: sendMessage,
                     backgroundColor: ColorStyles.mainColor,
                     foregroundColor: ColorStyles.white,
                     borderColor: ColorStyles.mainColor,
