9bae0711c56ccfdf9a3c75583cbb18df4c34e0bc
uraflower <82873315+uraflower@users.noreply.github.com>
2023-05-27 00:36:00 +0900

    🔨 Update_Remove onboarding page in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:brainstorm_meokjang/app_pages_container.dart';
 import 'package:brainstorm_meokjang/firebase_options.dart';
-import 'package:brainstorm_meokjang/pages/start/onboarding_page.dart';
 import 'package:brainstorm_meokjang/utilities/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -74,7 +73,7 @@ class _MyAppState extends State<MyApp> {
           color: ColorStyles.iconColor,
         ),
       ),
-      home: isMeokjangUser ? const AppPagesContainer() : const OnboardingPage(),
+      home: const AppPagesContainer(),
     );
   }
 }
