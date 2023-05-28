a07aadaacb95e40fc7491c112858b6bc6601a207
uraflower <82873315+uraflower@users.noreply.github.com>
2023-05-28 23:57:51 +0900

    ➕ Add_Add toJson, fromJson method in Message model

--- lib/models/chat_message.dart
+++ lib/models/chat_message.dart
@@ -15,4 +15,22 @@ class Message {
     required this.message,
     required this.date,
   });
+
+  Map<String, String> toJson() {
+    final Map<String, String> data = <String, String>{};
+    data['type'] = type.name;
+    data['sender'] = sender.toString();
+    data['message'] = message;
+    data['date'] = date.toString();
+    return data;
+  }
+
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      type: MessageType.values.firstWhere((type) => type.name == json['type']),
+      sender: int.parse(json['sender']),
+      message: json['message'],
+      date: DateTime.parse(json['date']),
+    );
+  }
 }
