272914ecfb8eba0e219ed49c66fc19e1216d03ed
MinJu Jo <pida6716@gmail.com>
2023-06-03 21:05:32 +0900

    💡 Comment 22.01

--- lib/providers/notification_Controller.dart
+++ lib/providers/notification_Controller.dart
@@ -3,14 +3,12 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationController extends GetxController {
-  // 메시징 서비스 기본 객체 생성
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   @override
   void onInit() async {
-    /// 첫 빌드시, 권한 확인하기
-    /// 아이폰은 무조건 받아야 하고, 안드로이드는 상관 없음. 따로 유저가 설정하지 않는 한,
-    /// 자동 권한 확보 상태
+    // 첫 빌드시, 권한 확인
+    // 아이폰은 무조건 받아야 하고, 안드로이드는 상관 없음. 따로 유저가 설정하지 않는 한,
     NotificationSettings settings = await messaging.requestPermission(
       alert: true,
       announcement: false,
@@ -21,12 +19,12 @@ class NotificationController extends GetxController {
       sound: true,
     );
     print(settings.authorizationStatus);
-    _getToken();
+    //_getToken();
     _onMessage();
     super.onInit();
   }
 
-  /// 디바이스 고유 토큰을 얻기 위한 메소드, 처음 한번만 사용해서 토큰 확보
+  //토큰 얻는 메소드, 회원가입시 한번만 되도록 수정하기
   void _getToken() async {
     String? token = await messaging.getToken();
     try {
@@ -34,45 +32,32 @@ class NotificationController extends GetxController {
     } catch (e) {}
   }
 
-  /// ----------------------------------------------------------------------------
-
-  /// * 안드로이드에서 foreground 알림 위한 flutter_local_notification 라이브러리 *
-  /// 1. channel 생성 (우리의 알림을 따로 전달해줄 채널을 직접 만든다)
-  /// 2. 그 채널을 우리 메인 채널로 정해줄 플러그인을 만들어준다.
-
-  // 1.
   final AndroidNotificationChannel channel = const AndroidNotificationChannel(
     'high_importance_channel', // id
     'High Importance Notifications', // title
     description: 'This channel is used for important notifications.', // description
     importance: Importance.max,
   );
-  // 2.
+
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
   void _onMessage() async {
-    /// 1. 위에서 생성한 channel 을 플러그인 통해 메인 채널로 설정한다.
-    /// 2. 플러그인을 초기화하여 추가 설정을 해준다.
-
-    // 1.
     await flutterLocalNotificationsPlugin
         .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
         ?.createNotificationChannel(channel);
-    // 2.
     await flutterLocalNotificationsPlugin.initialize(
         const InitializationSettings(
             android: AndroidInitializationSettings('@mipmap/ic_launcher'),
             iOS: DarwinInitializationSettings()),
         onDidReceiveNotificationResponse: (details) => {});
 
-    // 1. 콘솔에서 발송하는 메시지를 message 파라미터로 받아온다.
-    /// 메시지가 올 때마다 listen 내부 콜백이 실행된다.
+    /// 메시지가 올 때마다 listen 내부 콜백 실행.
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       RemoteNotification? notification = message.notification;
       AndroidNotification? android = message.notification?.android;
 
-      // android 일 때만 flutterLocalNotification 을 대신 보여주는 거임. 그래서 아래와 같은 조건문 설정.
+      // android 일 때만
       if (notification != null && android != null) {
         flutterLocalNotificationsPlugin.show(
           notification.hashCode,
@@ -83,15 +68,14 @@ class NotificationController extends GetxController {
                 channelDescription: channel.description),
           ),
 
-          // 넘겨줄 데이터가 있으면 아래 코드를 써주면 됨.
+          // 넘길 데이터 있으면 아래코드 쓰기.
           // payload: message.data['argument']
         );
       }
-      // 개발 확인 용으로 print 구문 추가
+
+      //데이터 잘 받는지 테스트용 코드
       print('foreground 상황에서 메시지를 받았다.');
-      // 데이터 유무 확인
       print('Message data: ${message.data}');
-      // notification 유무 확인
       if (message.notification != null) {
         print('Message also contained a notification: ${message.notification!.body}');
       }
