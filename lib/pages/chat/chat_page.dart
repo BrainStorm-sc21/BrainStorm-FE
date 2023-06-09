889dbd331f59c716ef628e1eadb3dce746b4087f
uraflower <82873315+uraflower@users.noreply.github.com>
2023-06-09 19:02:57 +0900

    ♻️ Refactor 14.02_Manage state using chat controller

--- lib/pages/chat/chat_page.dart
+++ lib/pages/chat/chat_page.dart
@@ -1,9 +1,8 @@
-import 'package:brainstorm_meokjang/models/chat_room.dart';
+import 'package:brainstorm_meokjang/providers/chat_controller.dart';
 import 'package:brainstorm_meokjang/utilities/colors.dart';
-import 'package:brainstorm_meokjang/utilities/domain.dart';
 import 'package:brainstorm_meokjang/widgets/enter_chat/enter_chat_widget.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ChatPage extends StatefulWidget {
   final int userId;
@@ -17,52 +16,12 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  late List<Room> roomList = List.empty(growable: true);
+  late final ChatController _chatController = Get.put(ChatController());
 
   @override
   void initState() {
     super.initState();
-    loadChatRoomList();
-  }
-
-  Future<void> loadChatRoomList() async {
-    Dio dio = Dio();
-    dio.options
-      ..baseUrl = baseURI
-      ..connectTimeout = const Duration(seconds: 5)
-      ..receiveTimeout = const Duration(seconds: 10);
-
-    final Response res = await dio.get('/chat/room/${widget.userId}');
-
-    try {
-      if (res.data['status'] == 200) {
-        print('채팅 목록 로드 성공!!:\n ${res.data['data']}');
-        List<dynamic> jsonList = res.data['data'] as List;
-        setState(() {
-          roomList = jsonList.map((data) => Room.fromJson(data)).toList();
-        });
-        sortRoomListByNewest();
-      } else {
-        print('채팅 목록 로드 실패!!');
-        throw Exception();
-      }
-    } catch (e) {
-      debugPrint('$e');
-    } finally {
-      dio.close();
-    }
-  }
-
-  void sortRoomListByNewest() {
-    roomList.sort((b, a) {
-      if (b.lastTime == null) {
-        return 1;
-      } else if (a.lastTime == null) {
-        return 0;
-      } else {
-        return a.lastTime!.compareTo(b.lastTime!);
-      }
-    });
+    _chatController.loadChatRoomList(widget.userId);
   }
 
   @override
@@ -89,7 +48,7 @@ class _ChatPageState extends State<ChatPage> {
               padding: EdgeInsets.symmetric(vertical: 20),
               child: GoRecipe(),
             ),
-            roomList.isEmpty
+            _chatController.roomList.isEmpty
                 ? const Expanded(
                     child: Center(
                       child: Text('같이먹장에서 다른 사용자와 대화를 시작해보세요!'),
@@ -97,14 +56,15 @@ class _ChatPageState extends State<ChatPage> {
                   )
                 : Expanded(
                     child: ListView.builder(
-                      itemCount: roomList.length,
+                      itemCount: _chatController.roomList.length,
                       itemBuilder: (context, index) {
                         return ChatUnit(
                           senderId: widget.userId,
-                          receiverId: roomList[index].sender == widget.userId
-                              ? roomList[index].receiver
-                              : roomList[index].sender,
-                          room: roomList[index],
+                          receiverId: _chatController.roomList[index].sender ==
+                                  widget.userId
+                              ? _chatController.roomList[index].receiver
+                              : _chatController.roomList[index].sender,
+                          room: _chatController.roomList[index],
                         );
                       },
                     ),
