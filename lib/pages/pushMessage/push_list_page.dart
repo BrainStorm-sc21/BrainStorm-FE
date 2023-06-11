35eb854db2dafa17a5fc56977d542ddc0f1d6a3a
joongwon <wnddnjs823@ajou.ac.kr>
2023-06-12 06:30:43 +0900

    🔨 Update Temporary push page

--- lib/pages/pushMessage/push_list_page.dart
+++ lib/pages/pushMessage/push_list_page.dart
@@ -38,10 +38,111 @@ class PushList extends StatelessWidget {
                   color: ColorStyles.lightgrey,
                   thickness: 1.5,
                   endIndent: 10),
+              Expanded(
+                child: TabBarView(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Container(
+                      child: ListView.builder(
+                        itemCount: 5,
+                        itemBuilder: (context, index) {
+                          return const PushUnit(
+                              type: '소비기한',
+                              time: '1분 전',
+                              title: '눈을 감자의 소비기한이 2일 남았습니다!');
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Container(
+                      child: ListView.builder(
+                        itemCount: 5,
+                        itemBuilder: (context, index) {
+                          return const PushUnit(
+                              type: '채팅',
+                              time: '3분 전',
+                              title: '먹짱2호 님에게 채팅이 왔습니다!!');
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Container(
+                      child: ListView.builder(
+                        itemCount: 5,
+                        itemBuilder: (context, index) {
+                          return const PushUnit(
+                              type: '리뷰',
+                              time: '5분 전',
+                              title: '먹짱3호님이 리뷰를 남기셨습니다!!!');
+                        },
+                      ),
+                    ),
+                  ),
+                ]),
+              )
             ],
           )
           //const Center(child: Text("아무 알림이 없습니다")),
           ),
+    );
+  }
+}
+
+class PushUnit extends StatelessWidget {
+  final String type;
+  final String time;
+  final String title;
+  const PushUnit(
+      {super.key, required this.type, required this.time, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: InkWell(
+        onTap: () {
+          print('푸시알림 클릭!');
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: ColorStyles.lightGrey,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          width: double.infinity,
+          height: 70,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(type,
+                        style: const TextStyle(fontWeight: FontWeight.w500)),
+                    const SizedBox(width: 10),
+                    Text(
+                      time,
+                      style: const TextStyle(fontSize: 13),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  title,
+                  style: const TextStyle(
+                      color: ColorStyles.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
