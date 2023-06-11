498c942edb3df91446b1ae6df0032fae513a7a92
joongwon <wnddnjs823@ajou.ac.kr>
2023-06-12 06:01:23 +0900

    ➕ Add tabbarController in push page

--- lib/pages/pushMessage/push_list_page.dart
+++ lib/pages/pushMessage/push_list_page.dart
@@ -1,15 +1,47 @@
 import 'package:brainstorm_meokjang/utilities/Colors.dart';
 import 'package:flutter/material.dart';
 
-class PushList extends StatelessWidget{
+class PushList extends StatelessWidget {
   const PushList({super.key});
-  
+
+  TabBar get _tabBar => const TabBar(
+          padding: EdgeInsets.only(top: 10),
+          isScrollable: false,
+          indicatorColor: ColorStyles.mainColor,
+          indicatorWeight: 4,
+          labelColor: ColorStyles.mainColor,
+          unselectedLabelColor: ColorStyles.textColor,
+          labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          tabs: [
+            Tab(text: "소비 기한"),
+            Tab(text: "채팅"),
+            Tab(
+              text: "리뷰",
+            )
+          ]);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("알림"),centerTitle: true, backgroundColor: ColorStyles.mainColor,),
-      body: const Center(child:Text("아무 알림이 없습니다")),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text("알림"),
+            centerTitle: true,
+            backgroundColor: ColorStyles.mainColor,
+          ),
+          body: Column(
+            children: [
+              _tabBar,
+              const Divider(
+                  height: 0,
+                  color: ColorStyles.lightgrey,
+                  thickness: 1.5,
+                  endIndent: 10),
+            ],
+          )
+          //const Center(child: Text("아무 알림이 없습니다")),
+          ),
     );
   }
-
 }
