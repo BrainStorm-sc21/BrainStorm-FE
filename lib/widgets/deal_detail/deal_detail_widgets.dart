67cac3adc9cc955183a1e9243e3ef28fa67ac8d2
joongwon <wnddnjs823@ajou.ac.kr>
2023-06-12 02:08:14 +0900

    🔨 Update Request complete deal and review

--- lib/widgets/deal_detail/deal_detail_widgets.dart
+++ lib/widgets/deal_detail/deal_detail_widgets.dart
@@ -57,33 +57,6 @@ class _TopPostUnitState extends State<TopPostUnit> {
     }
   }
 
-  void requestCompleteDeal() async {
-    Dio dio = Dio();
-    dio.options
-      ..baseUrl = baseURI
-      ..connectTimeout = const Duration(seconds: 5)
-      ..receiveTimeout = const Duration(seconds: 10);
-
-    print("딜 아이디: ${widget.dealId}");
-
-    try {
-      final resp = await dio.put('deal/${widget.dealId}/complete');
-      print("Modify Status: ${resp.statusCode}");
-
-      //Navigator.pop(context);
-
-      if (resp.statusCode == 200) {
-        showToast('해당 거래가 완료되었습니다');
-      } else {
-        print('??');
-      }
-    } catch (e) {
-      Exception(e);
-    } finally {
-      dio.close();
-    }
-  }
-
   @override
   void initState() {
     super.initState();
@@ -206,9 +179,7 @@ class _TopPostUnitState extends State<TopPostUnit> {
               ),
               // 확인 버튼
               TextButton(
-                onPressed: () {
-                  requestCompleteDeal();
-                },
+                onPressed: () {},
                 child: const Text(
                   "확인",
                   style: TextStyle(color: Colors.pink),
