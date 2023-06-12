e2bedfd9549749d24a672400721f2c5ec52aa521
joongwon <wnddnjs823@ajou.ac.kr>
2023-06-12 12:20:55 +0900

    🔨 Update push model

--- lib/models/push.dart
+++ lib/models/push.dart
@@ -1,1 +1,23 @@
-class Push {}
+class Push {
+  int noticeId;
+  String title;
+  String body;
+  String type;
+  String typeId;
+  Push(
+      {required this.noticeId,
+      required this.title,
+      required this.body,
+      required this.type,
+      required this.typeId});
+
+  factory Push.fromJson(Map<String, dynamic> json) {
+    return Push(
+      noticeId: json['noticeId'],
+      title: json['title'],
+      body: json['body'],
+      type: json['type'],
+      typeId: json['typeId'],
+    );
+  }
+}
